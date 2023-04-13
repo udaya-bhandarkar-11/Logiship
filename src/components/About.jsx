56a2e28aa0153eebc59fefd59a55465439347e9f
850066884Yatsha <850066884@genpact.com>
2023-04-13 23:46:13 +0530

    spell check

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
       <motion.p variants={fadeIn("","","0.1",1)} 
       className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-      The following features are provided to amke it convinient </motion.p>
+      The following features are provided to make your transports better and faster.  </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
